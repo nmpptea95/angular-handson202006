0e7730c3591aa503e90f4f59f1ebd9a1b0f104e2
nmpptea95 <m.n.popoteaymym@gmail.com>
2020-06-21 18:26:30 +0900

    step2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from "@angular/core";
+import { todoList } from "./todo-list";
+import { Todo } from "./todo";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  title = 'My todo-list';
+export class AppComponent {
+  title = "My todo-list";
+  todoList: Todo[] = [...todoList];
 }
---
+++ src/app/todo-list.ts
@@ -0,0 +1,16 @@
+import { Todo } from './todo';
+
+export const todoList: Todo[] = [
+  {
+    title: 'Get started Angular',
+    completed: false
+  },
+  {
+    title: 'Write an article',
+    completed: false 
+  },
+  {
+    title: 'Get the hikarie',
+    completed: true
+  }
+];
